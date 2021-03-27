b19550429ab9059b5e908f6eb12c1e2c5a63c7fa
Andreas Beuger <andreasbeuger@gmail.com>
2021-03-27 21:00:29 +0100

    session route

--- server/routes/motogp-route.js
+++ server/routes/motogp-route.js
@@ -8,6 +8,8 @@ const router = express.Router()
 router.get('/standings', motogpController.getLatest)
 router.get('/races', motogpController.getRaces)
 router.get('/categories', motogpController.getCategories)
+router.get('/sessions', motogpController.getSessions)
+router.get('/session', motogpController.getSession)
 
 // Export router
 module.exports = router
