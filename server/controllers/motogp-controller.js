e399aa0756279faac5850949ab5a2e9508340af2
Andreas Beuger <andreasbeuger@gmail.com>
2021-05-25 23:32:19 +0200

    Fixed naming of endpoints and added endpoint for getting the latest race

--- server/controllers/motogp-controller.js
+++ server/controllers/motogp-controller.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-exports.getLatest = async (req, res) => {
+exports.getStandings = async (req, res) => {
 
     let year = req.query.year;
     let race = req.query.race;
@@ -8,6 +8,16 @@ exports.getLatest = async (req, res) => {
     let session = req.query.session ? req.query.session : 'RAC';
 
     let latestPath = `https://www.motogp.com/en/Results+Statistics/${year}/${race}/${category}/${session}/World+Standing`;
+
+    fetch(latestPath)
+        .catch(err => res.send({status: 500, data: `Request to url ${latestPath} failed`}))
+        .then(res => res.text())
+        .then(body => res.send({status: 200, data: body}))
+}
+
+exports.getLatestRace = async (req, res) => {
+
+    let latestPath = `https://www.motogp.com/en/Results+Statistics`;
 
     fetch(latestPath)
         .catch(err => res.send({status: 500, data: `Request to url ${latestPath} failed`}))
--- server/routes/motogp-route.js
+++ server/routes/motogp-route.js
@@ -5,11 +5,12 @@ const motogpController = require('./../controllers/motogp-controller.js')
 // Create express router
 const router = express.Router()
 
-router.get('/standings', motogpController.getLatest)
+router.get('/standings', motogpController.getStandings)
 router.get('/races', motogpController.getRaces)
 router.get('/categories', motogpController.getCategories)
 router.get('/sessions', motogpController.getSessions)
 router.get('/session', motogpController.getSession)
+router.get('/latest', motogpController.getLatestRace)
 
 // Export router
 module.exports = router
