784273f8d408d0e5458ceaa57bcf878a694e6430
Andreas Beuger <andreasbeuger@gmail.com>
2021-06-07 16:02:07 +0200

    Add build folder

---
+++ server/build/precache-manifest.0527a358c387fd16f34ceb9b1405a2c1.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9b215fa55a7454b052dc081af0daec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6589a46cca35c2e5d4c0",
+    "url": "/static/css/main.22b78b3a.chunk.css"
+  },
+  {
+    "revision": "6e326d4700051f3a240f",
+    "url": "/static/js/2.98a1ae5a.chunk.js"
+  },
+  {
+    "revision": "e79898a42ec2d02d5bbd03509f17e33d",
+    "url": "/static/js/2.98a1ae5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6589a46cca35c2e5d4c0",
+    "url": "/static/js/main.3051ded4.chunk.js"
+  },
+  {
+    "revision": "20810ff1260629a634e6",
+    "url": "/static/js/runtime-main.4bc2bb2f.js"
+  },
+  {
+    "revision": "74087b9c93e9bab4f2c29aa341995da4",
+    "url": "/static/media/about-bg.74087b9c.svg"
+  },
+  {
+    "revision": "e1b62b6d3a80e8847c74eb563eac5a10",
+    "url": "/static/media/aragon.e1b62b6d.svg"
+  },
+  {
+    "revision": "f00e7fbb7d4e63f0d49a3efa9e357f68",
+    "url": "/static/media/argentina.f00e7fbb.svg"
+  },
+  {
+    "revision": "45bab394bc2c5d509a4dbe8afbea4406",
+    "url": "/static/media/assen.45bab394.svg"
+  },
+  {
+    "revision": "c915049cad487812fffb11dc8167e678",
+    "url": "/static/media/catalunya.c915049c.svg"
+  },
+  {
+    "revision": "79caef2135c6c9e892619de81282b34b",
+    "url": "/static/media/chang.79caef21.svg"
+  },
+  {
+    "revision": "8b4ab74a96ea7701077e697323ddc926",
+    "url": "/static/media/cota.8b4ab74a.svg"
+  },
+  {
+    "revision": "c64f6f2423a6c20396599ecb6e583a5e",
+    "url": "/static/media/fabio-tinted.c64f6f24.png"
+  },
+  {
+    "revision": "fa0e069ccbc2ab61f3646bda7e9a7ac7",
+    "url": "/static/media/jerez.fa0e069c.svg"
+  },
+  {
+    "revision": "02455d845181f01e8e7bb9d6539eed49",
+    "url": "/static/media/le-mans.02455d84.svg"
+  },
+  {
+    "revision": "20fca82e4be1bbf127538da617ffb19a",
+    "url": "/static/media/misano.20fca82e.svg"
+  },
+  {
+    "revision": "44b4a106e9f6531c59b87c70d31a423a",
+    "url": "/static/media/motegi.44b4a106.svg"
+  },
+  {
+    "revision": "10c69c69e5231db8fed671a2f1675d48",
+    "url": "/static/media/mugello.10c69c69.svg"
+  },
+  {
+    "revision": "b6613a233b0a2cff96c0559b614d181d",
+    "url": "/static/media/philip-island.b6613a23.svg"
+  },
+  {
+    "revision": "69f01b31874ce8bbccaf939d126d8caf",
+    "url": "/static/media/qatar.69f01b31.svg"
+  },
+  {
+    "revision": "12a3d6f2731b0572a058f95412d35d62",
+    "url": "/static/media/rain.12a3d6f2.svg"
+  },
+  {
+    "revision": "8df297fc053dc5a9eb3ec96899b97131",
+    "url": "/static/media/redbullring.8df297fc.svg"
+  },
+  {
+    "revision": "ff6becd2079ebe75813f0c81f0223c03",
+    "url": "/static/media/ricardo-tormo.ff6becd2.svg"
+  },
+  {
+    "revision": "f18deb4e1f351adc5e5f3836bf02f629",
+    "url": "/static/media/sachsenring.f18deb4e.svg"
+  },
+  {
+    "revision": "a20e7dfe4c4eeef1b83a6b3965bfbf18",
+    "url": "/static/media/sepang.a20e7dfe.svg"
+  },
+  {
+    "revision": "fef5bb7404f7b4fad809957cd1d3061f",
+    "url": "/static/media/silverstone.fef5bb74.svg"
+  },
+  {
+    "revision": "065060cb626cbf16e60693f8c0ee5cc1",
+    "url": "/static/media/sun.065060cb.svg"
+  },
+  {
+    "revision": "7169ad23586a5dc96d0a178ca55ea413",
+    "url": "/static/media/wheel.7169ad23.svg"
+  }
+]);
