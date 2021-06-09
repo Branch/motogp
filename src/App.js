1beabee11c781a96d7cc66a559864b19cd924729
Andreas Beuger <andreasbeuger@gmail.com>
2021-06-09 16:43:12 +0200

    UA id

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from './views/about';
 import Sidebar from './components/sidebar';
 import GoToTop from './components/goToTop';
 import ReactGA from 'react-ga';
-ReactGA.initialize('275249294');
+ReactGA.initialize('UA-129307485-2');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 // Create App component
